19a5454e5d55575c31ab5e429100a1ec48fde20f
Jose Angel Morena <jamorena@essiprojects.com>
2020-02-14 10:29:19 +0100

    script modified

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sed -i "s/host_name.*/host_name $(hostname)/g" /usr/local/nagios/etc/objects/localhost.cfgsed -i "s/host_name.*/host_name $(hostname)/g" /usr/local/nagios/etc/objects/localhost.cfg  
 /etc/rc.d/init.d/nagios start
 /usr/sbin/httpd -k start
 tail -f /var/log/httpd/access_log /var/log/httpd/error_log
